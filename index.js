920b82ea657f3cc674cf6b3db1f3830d06dd20b6
maclaine <maclainekuria@gmail.com>
2022-06-17 15:37:48 +0300

    commit tree 85e6f7d6d00df87be9bc6740456263d902673dd2

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in seenNumbers) return true;
+    seenNumbers[number] = true;
+  }
+
+  return false;
+}
+
+function findSock(array) {
+  for (const item of array) {
+    if (item === "sock") return "sock";
+  }
+}
+
+function findSock(object) {
+  if (object.sock) return "sock";
 }
 
 /* 
